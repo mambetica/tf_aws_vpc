00f300dd0ba0d574af754513767d335fbcfc58d5
Andy A <mambetica@hotmail.com>
2016-05-28 18:28:08 +0100

    Output public and private subnets

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,9 @@
 output "vpc_id" {
   value = "${aws_vpc.vpc.id}"
 }
+output "private_subnets" {
+  value = "${join(",", aws_subnet.private.*.id)}"
+}
+output "public_subnets" {
+  value = "${join(",", aws_subnet.public.*.id)}"
+}
