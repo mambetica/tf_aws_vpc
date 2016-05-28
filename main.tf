13558cb7b2be3b3c42d9561c7b48cf9a51d0c281
Andy A <mambetica@hotmail.com>
2016-05-28 15:25:53 +0100

    Add routing

--- main.tf
+++ main.tf
@@ -19,6 +19,28 @@ resource "aws_internet_gateway" "ig" {
   }
 }
 
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags {
+    Name = "${var.name}-public"
+    Owner = "${var.owner}"
+  }
+}
+
+resource "aws_route" "public_internet_gateway" {
+    route_table_id = "${aws_route_table.public.id}"
+    destination_cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.ig.id}"
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  tags {
+    Name = "${var.name}-private"
+    Owner = "${var.owner}"
+  }
+}
+
 resource "aws_subnet" "private" {
   vpc_id = "${aws_vpc.vpc.id}"
   cidr_block = "${element(split(",", var.subnets_private), count.index)}"
@@ -40,4 +62,16 @@ resource "aws_subnet" "public" {
   }
   count = "${length(compact(split(",", var.subnets_public)))}"
   map_public_ip_on_launch = true
+}
+
+resource "aws_route_table_association" "private" {
+  count = "${length(compact(split(",", var.subnets_private)))}"
+  subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
+  route_table_id = "${aws_route_table.private.id}"
+}
+
+resource "aws_route_table_association" "public" {
+  count = "${length(compact(split(",", var.subnets_public)))}"
+  subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+  route_table_id = "${aws_route_table.public.id}"
 }
