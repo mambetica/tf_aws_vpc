0bb724245e07e185c046944b5bad23a18d8c3cd3
Andy A <mambetica@hotmail.com>
2016-05-28 02:11:47 +0100

    Removed variable for provider name

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_vpc" "${var.name}" {
+resource "aws_vpc" "vpc" {
   cidr_block = "${var.cidr_block}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support = "${var.enable_dns_support}"
