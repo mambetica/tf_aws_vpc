7e86d8d18ac377e40e1ec7fbafcada3edf67794c
Andy A <mambetica@hotmail.com>
2016-05-30 18:18:22 +0100

    Add AZs to output

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,9 @@ output "private_subnets" {
 output "public_subnets" {
   value = "${join(",", aws_subnet.public.*.id)}"
 }
+output "private_availability_zones" {
+  value = "${join(",", aws_subnet.private.*.availability_zone)}"
+}
+output "public_availability_zones" {
+  value = "${join(",", aws_subnet.public.*.availability_zone)}"
+}
